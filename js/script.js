49e53709608b389619ab1c34dd784ed320a167c1
Nathan Childress <themirth@gmail.com>
2020-07-13 14:05:20 -0700

    Adding in some blocks so I can try and see the layout

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const marsWeatherURL = "";
+const spaceXURL = "";
+const launchAPI = "https://launchlibrary.net/1.3/launch/next/5"
+
+let $roverTweet = roverTweets.$insight
+$roverTweet.appendTo($("#twitterfeed"));
+
